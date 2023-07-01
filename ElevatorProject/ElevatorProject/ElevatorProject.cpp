4be23cfe7d3bdae661c1df93ef809ae3aecb65a4
Aarmn the limitless <aarmn80@gmail.com>
2023-07-01 13:35:34 +0000

    inital commit with solution imported to main codebase

---
+++ ElevatorProject/ElevatorProject/ElevatorProject.cpp
@@ -0,0 +1,17 @@
+/*
+ * ElevatorProject.cpp
+ *
+ * Created: 10/04/1402 04:11:45 ب.ظ
+ *  Author: aarmn
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
