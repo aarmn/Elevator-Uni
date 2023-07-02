cfb42b43f88f0c22d72104a7e66734254b2d13bb
MahyarRajaei <null>
2023-07-02 19:38:29 +0330

    trying to implement interrupts

--- ElevatorProject/ElevatorProject/ElevatorProject.cpp
+++ ElevatorProject/ElevatorProject/ElevatorProject.cpp
@@ -20,24 +20,25 @@
 
 
 
-/************* defining Motors **************/
-#define LIFT_MOTOR_DDR DDRA
-#define LIFT_MOTOR_PORT PORTA
-#define LIFT_MOTOR_IN1 PA1
-#define LIFT_MOTOR_IN2 PA2
-#define LIFT_MOTOR_EN PA3
+/************* defining Ports **************/
+#define LIFT_MOTOR_DDR DDRC
+#define LIFT_MOTOR_PORT PORTC
+#define LIFT_MOTOR_IN1 PC1
+#define LIFT_MOTOR_IN2 PC2
+#define LIFT_MOTOR_EN PC3
 
-#define DOOR_MOTOR_DDR DDRA 
-#define DOOR_MOTOR_PORT PORTA
-#define DOOR_MOTOR_IN1 PA4
-#define DOOR_MOTOR_IN2 PA5
-#define DOOR_MOTOR_EN PA6
+#define DOOR_MOTOR_DDR DDRC 
+#define DOOR_MOTOR_PORT PORTC
+#define DOOR_MOTOR_IN1 PC4
+#define DOOR_MOTOR_IN2 PC5
+#define DOOR_MOTOR_EN PC6
 
-#define KEY_SET_DDR DDRD
-#define KEY_SET_PORT PORTD
-#define KEY_SET_I1 PD0
-#define KEY_SET_I2 PD1
-#define KEY_SET_I3 PD3
+#define KEY_SET_DDR DDRA
+#define KEY_SET_PORT PORTA
+#define KEY_SET_I1 PA0
+#define KEY_SET_I2 PA1
+#define KEY_SET_I3 PA2
+#define KEY_SET_I4 PA3
 
 #define INTERRUPT PD2
 
@@ -49,15 +50,16 @@
 
 //request code is 4bit long
 //#define NO_REQUEST_CODE 0b0000
-#define FLOOR_1_REQUEST_CODE 0b0000
-#define FLOOR_2_REQUEST_CODE 0b0001
-#define FLOOR_3_REQUEST_CODE 0b0010
-#define FLOOR_4_REQUEST_CODE 0b0011
-#define KEY_PAD_1_REQUEST_CODE 0b0100
-#define KEY_PAD_2_REQUEST_CODE 0b0101
-#define KEY_PAD_3_REQUEST_CODE 0b0110
-#define KEY_PAD_4_REQUEST_CODE 0b0111
-#define KEY_PAD_ALARM_REQUEST_CODE 0b1111
+#define FLOOR_0_REQUEST_CODE 0b0000
+#define FLOOR_1_REQUEST_CODE 0b0001
+#define FLOOR_2_REQUEST_CODE 0b0010
+#define FLOOR_3_REQUEST_CODE 0b0011
+#define KEY_PAD_0_REQUEST_CODE 0b0100
+#define KEY_PAD_1_REQUEST_CODE 0b0101
+#define KEY_PAD_2_REQUEST_CODE 0b0110
+#define KEY_PAD_3_REQUEST_CODE 0b0111
+#define KEY_PAD_ALARM_REQUEST_CODE 0b1000
+#define KEY_PAD_BIT_MSK 0b00001111
 
 
 enum direction {
@@ -74,14 +76,14 @@ enum State {
 typedef int floor_t;
 
 State state = STATIONARY;
-floor_t current_floor = 1;
+floor_t current_floor = 0;
 
 
 
 
 struct request_holder{
 	//int* priotity  = (int*) malloc(4 * sizeof(int));
-	uint8_t number_of_avialable_requests = 0;
+	int number_of_avialable_requests = 0;
 	bool request_for_floor[4] = {false , false, false, false};
 
 } request_holder;
@@ -91,37 +93,45 @@ void set_request_for_floor(floor_t fl){
 		request_holder.number_of_avialable_requests++;
 }
 
-	floor_t get_next_available_request(floor_t current_floor){
-		if (request_holder.number_of_avialable_requests == 0)
-			return -1;
-		uint8_t next_floor_rank = 0;
-		current_floor = current_floor - 1; //current_floor_index
-		floor_t first_above_floor = NULL, first_below_floor = NULL;
-		for (floor_t f = current_floor + 1; f < NUMBER_OF_FLOORS; f++) {
-			if (request_holder.request_for_floor[f])
-				next_floor_rank++;
-			if (first_above_floor == NULL)
-				first_above_floor = f + 1; //plus 1, to convert floor index to floor number
+floor_t get_nearest_available_request(floor_t current_floor){
+	
+	if (request_holder.number_of_avialable_requests <= 0)
+		return -1;
+
+	uint8_t below_distance = 0, above_distance = 0;
+	floor_t above_floor = -1;
+	for (floor_t f = current_floor; f < NUMBER_OF_FLOORS; f++) 
+		if (request_holder.request_for_floor[f]) {
+			above_floor = f;
+			above_distance = f - current_floor;
+			break;
 		}
-				
-		for (floor_t f = current_floor - 1; f >= 0 ; f--)  {
-			if (request_holder.request_for_floor[f])
-				next_floor_rank++;
-			if (first_below_floor == NULL)
-				first_below_floor = f + 1; //plus 1, to convert floor index to floor number
+	PORTB = above_distance;
+	_delay_ms(1000);
 
-		}
-
-
-		request_holder.number_of_avialable_requests--;
-		if (next_floor_rank > 0) {
-			request_holder.request_for_floor[first_above_floor] = false;
-			return first_above_floor;
-		} else {
-			request_holder.request_for_floor[first_below_floor] = false;
-			return first_below_floor;
+	floor_t below_floor = -1;
+	for (floor_t f = current_floor - 1; f >= 0; f--)
+		if (request_holder.request_for_floor[f]) {
+			below_floor = f;
+			below_distance = current_floor - f; 
+			break;
 		}
 
+	PORTB = below_distance;
+	_delay_ms(1000);
+	
+	request_holder.number_of_avialable_requests--;
+	if (below_distance > above_distance && above_floor != -1) {
+		request_holder.request_for_floor[above_floor] = false;
+		return above_floor;
+	} else if (below_distance < above_distance && below_floor != -1) {
+		request_holder.request_for_floor[below_floor] = false;
+		return below_floor;
+	} else {
+		request_holder.request_for_floor[current_floor] = false;
+		return current_floor;
+	}
+	
 }
 
 void lift_motor_request(floor_t current_floor, floor_t dest_floor);
@@ -134,13 +144,20 @@ void rotate_lift_motor_for_one_floor(direction);
 void open_the_door();
 void close_the_door();
 
+void init_ddr();
+
 
 
 ISR(INT0_vect) {
 
-	volatile uint8_t floor_request_code = PIND; 
-	switch (floor_request_code) {
+	volatile uint8_t request_code = PINA & KEY_PAD_BIT_MSK; 
+	//PORTB = request_code;
+	switch (request_code) {
 
+		case KEY_PAD_0_REQUEST_CODE:
+		case FLOOR_0_REQUEST_CODE:
+			set_request_for_floor(0);
+		break;
 		case KEY_PAD_1_REQUEST_CODE:
 		case FLOOR_1_REQUEST_CODE:
 			set_request_for_floor(1);
@@ -153,21 +170,13 @@ ISR(INT0_vect) {
 		case FLOOR_3_REQUEST_CODE:
 			set_request_for_floor(3);
 		break;
-		case KEY_PAD_4_REQUEST_CODE:
-		case FLOOR_4_REQUEST_CODE:
-			set_request_for_floor(4);
-		break;
 		case KEY_PAD_ALARM_REQUEST_CODE:
 		//TODO: alarm ?
 		break;
 
 	}
-
-	if (state == STATIONARY){
-		floor_t next_floor = get_next_available_request(current_floor);
-		lift_motor_request(current_floor, next_floor);
-	}
-
+	floor_t nearest_floor = get_nearest_available_request(current_floor);
+	PORTB = nearest_floor; 
 }
 
 
@@ -176,28 +185,14 @@ int main(void)
 {
 
 
-	DDRD |= (0 << PB2);
-	DDRB |= (1 << PB0);
-
-	current_floor = 1;
-	set_request_for_floor(3);
-	set_request_for_floor(2);
-	set_request_for_floor(4);
-
-	current_floor = get_next_available_request(current_floor);
-	if (current_floor == 2)
-		PORTB |= (1 << PB0);
-
-
-	
-	GICR |= (1 << INT0);   //Enable External Interrupts INT0 and INT1
-	MCUCR=0x08;  //Configure INT0 active low level triggered and INT1 as falling edge
-
-	sei();     // Enable global interrupts by setting global interrupt enable bit in SREG
-
+	init_ddr();
+	DDRB = 0XFF;
 	while(1) {
-		PORTB |= 0B00000000;
-		
+		current_floor = 4;
+		set_request_for_floor(0);
+		//PORTB = get_nearest_available_request(0);
+		//PORTB = 10;
+
 	}
 }
 
@@ -207,18 +202,27 @@ void init_ddr(){
 	LIFT_MOTOR_DDR |= (1 << LIFT_MOTOR_IN1) | (1 << LIFT_MOTOR_IN2) | (1 << LIFT_MOTOR_EN);
 	DOOR_MOTOR_DDR |= (1 << DOOR_MOTOR_IN1) | (1 << DOOR_MOTOR_IN2) | (1 << DOOR_MOTOR_EN);
 
+	KEY_SET_DDR &= ~(1 << KEY_SET_I1) & ~(1 << KEY_SET_I2) & ~(1 << KEY_SET_I4);
+
+
+	GICR |= (1 << INT0);   //Enable External Interrupts INT0 and INT1
+	MCUCR=0x08;  //Configure INT0 active low level triggered and INT1 as falling edge
+	sei();     // Enable global interrupts by setting global interrupt enable bit in SREG
+
 }
 
 
 
 void lift_motor_request(floor_t current_floor, floor_t dest_floor){
 	
+	state = MOVING;
 	int number_of_floors = current_floor - dest_floor;
 	direction dir = number_of_floors >= 0 ? ANTI_CLOCK_WISE : CLOCK_WISE; //specifing direction
 	number_of_floors = abs(number_of_floors); //specifing number of floors
 	for (int i = 0; i < number_of_floors; i++) 
 		lift_next_floor(current_floor, dir);
 		
+	state = STATIONARY;
 }
 
 
